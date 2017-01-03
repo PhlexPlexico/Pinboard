21a03e84fad119bc4f8ffa44e3a0e9c624a00e70
Evan <PhlexPlexico@users.noreply.github.com>
2017-01-03 00:05:27 -0600

    Update display.php

--- display.php
+++ display.php
@@ -147,12 +147,8 @@
 				// get the q parameter from URL
 				$servername = "localhost";
 				$username = "root";
-				$dbname = "pinboard";
-				$password = "qwertyhero";
-				//$servername = "lovett.usask.ca";
-				//$username = "cmpt350_amm215";
-				//$dbname = "cmpt350_amm215";
-				//$password = "5eukmjjz9w";
+				$dbname = "";
+				$password = "";
 				$email = $_SESSION["email"];
 				//create connection to the database
 				 try
